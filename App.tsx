effa50d0f840eaf9d89ecdcc488b33dc115b8806
devMorshed <ac3m0r@gmail.com>
2023-07-29 17:47:05 +0600

    Greetings added

--- App.tsx
+++ App.tsx
@@ -21,8 +21,11 @@ const App = (): JSX.Element => {
     <SafeAreaView>
       <ScrollView>
         <View style={styles.container}>
+          {/* Greetings */}
+          <View>
+            <Text style={styles.greetings}>Good Morning</Text>
+          </View>
           {/* Search Bar */}
-
           <View style={styles.search_bar}>
             <TextInput
               style={styles.search_input}
@@ -33,6 +36,24 @@ const App = (): JSX.Element => {
               placeholder="Search"
             />
           </View>
+
+          <ScrollView style={styles.tutorialTab} horizontal>
+            <View style={styles.tabItem}>
+              <Text style={styles.tabText}>For You</Text>
+            </View>
+            <View style={styles.tabItem}>
+              <Text style={styles.tabText}>All</Text>
+            </View>
+            <View style={styles.tabItem}>
+              <Text style={styles.tabText}>Cardio</Text>
+            </View>
+            <View style={styles.tabItem}>
+              <Text style={styles.tabText}>Full Body</Text>
+            </View>
+            <View style={styles.tabItem}>
+              <Text style={styles.tabText}>Yoga</Text>
+            </View>
+          </ScrollView>
 
           {/* Tutorial Card Section */}
           <View style={styles.tutorial_grid}>
@@ -65,6 +86,14 @@ const styles = StyleSheet.create({
     color: 'white',
   },
 
+  greetings: {
+    fontSize: 32,
+    lineHeight: 40,
+    fontWeight: '600',
+    paddingVertical: 24,
+    color: '#efefef',
+  },
+
   search_bar: {},
 
   search_input: {
@@ -72,6 +101,23 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 12,
+  },
+  tutorialTab: {
+    marginVertical: 24,
+  },
+  tabItem: {
+    backgroundColor: '#EEEEEE',
+    borderRadius: 8,
+    marginRight: 12,
+  },
+  tabText: {
+    color: '#616161',
+    fontSize: 12,
+    fontWeight: '600',
+    textAlign: 'center',
+    height: 36,
+    width: 74,
+    padding: 10,
   },
   tutorial_grid: {
     display: 'flex',
